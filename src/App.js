c4c47ee8e6e2815131d0a2e9249c142dc7a4eca7
sou21112 <162999453+sou21112@users.noreply.github.com>
2024-11-03 12:47:06 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// frontend/src/App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Sangrah</h1>
+        </div>
+    );
+}
+
+export default App;
