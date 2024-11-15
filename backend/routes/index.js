fc74562b702da8a779436a40b6109cb87a2f1ff3
sou21112 <162999453+sou21112@users.noreply.github.com>
2024-11-15 11:19:24 +0530

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// User routes
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
-
-module.exports = router;
