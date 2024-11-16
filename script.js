4d754b75ead5500be387e37527a8e770c8b12db4
sou21112 <162999453+sou21112@users.noreply.github.com>
2024-11-16 07:08:47 +0530

    Update and rename script.js to html

--- script.js
+++
@@ -1,8 +0,0 @@
-function togglePassword() {
-    var password = document.querySelector('input[type="password"]');
-    if (password.type === "password") {
-        password.type = "text";
-    } else {
-        password.type = "password";
-    }
-}
